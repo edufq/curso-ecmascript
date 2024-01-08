dfa989c6fdd825457f62674c4d8043a9bfe28f93
unknown <eduardofquintana@gmail.com>
2024-01-08 19:09:45 -0300

    lo que faltaba de la clase 4

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -26,6 +26,24 @@ let data = {id: 1, ...person, country};
 console.log(data);
 
 
+// rest
+
+function sum(num, ...values){
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(1, 1, 2, 3);
+
+
+
+
+
+
+
+
+
 
 
 

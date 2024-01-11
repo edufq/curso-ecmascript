ae3c2255c4d37d8631bf0c27cd70eef2e7970c93
edufq <eduardofquintana@gmail.com>
2024-01-10 22:05:38 -0300

    subo todos los modulos del curso

---
+++ src/es12/00-numeric-separators.js
@@ -0,0 +1,7 @@
+const value = 1_000_000_000_000;
+console.log(value);
+
+
+
+
+
---
+++ src/es12/01-replaceAll.js
@@ -0,0 +1,11 @@
+const string = "JavaScript es un maravilloso lenguaje de programacion, JavaScript";
+const replaceString1 = string.replace("JavaScript", "TypeScript");
+const replaceString2 = string.replaceAll("JavaScript", "TypeScript");
+
+console.log(replaceString1);
+console.log(replaceString2);
+
+
+
+
+
